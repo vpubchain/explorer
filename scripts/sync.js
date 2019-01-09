4258ff05db3aa7288b17a709103f6b6759aa27e7
lixu925 <lixu_11@yeah.net>
2019-01-09 00:40:26 -0800

    制作difficult图表

--- scripts/sync.js
+++ scripts/sync.js
@@ -163,7 +163,7 @@ is_locked(function (exists) {
                           }, function() {
                             console.log('index cleared (reindex)');
                           }); 
-                          db.update_tx_db(settings.coin, 1, stats.count, settings.update_timeout, function(){
+                          db.update_tx_db(settings.coin, 0, stats.count, settings.update_timeout, function(){
                             db.update_richlist('received', function(){
                               db.update_richlist('balance', function(){
                                 db.get_stats(settings.coin, function(nstats){
@@ -177,7 +177,7 @@ is_locked(function (exists) {
                       });
                     });              
                   } else if (mode == 'check') {
-                    db.update_tx_db(settings.coin, 1, stats.count, settings.check_timeout, function(){
+                    db.update_tx_db(settings.coin, 0, stats.count, settings.check_timeout, function(){
                       db.get_stats(settings.coin, function(nstats){
                         console.log('check complete (block: %s)', nstats.last);
                         exit();
